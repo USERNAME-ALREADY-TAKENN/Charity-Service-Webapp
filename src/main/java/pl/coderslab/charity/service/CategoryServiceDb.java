16e04909d5f8c08461ef4deff16156b7f4b01a50
woj ojojoj <50807860+USERNAME-ALREADY-TAKENN@users.noreply.github.com>
2020-11-16 15:58:05 +0100

    added donation service

--- src/main/java/pl/coderslab/charity/service/CategoryServiceDb.java
+++ src/main/java/pl/coderslab/charity/service/CategoryServiceDb.java
@@ -1,2 +1,23 @@
-package pl.coderslab.charity.service;public class CategoryServiceDb {
+package pl.coderslab.charity.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.repository.CategoryRepository;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceDb implements CategoryService {
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryServiceDb(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return this.categoryRepository.findAll();
+    }
 }
